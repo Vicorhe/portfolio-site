3acd7789238d734a07d81c0ed144dd23a9fa37ba
Victor He <vctr.he@gmail.com>
2020-04-29 08:51:09 -0700

    page heros, starting on entry component.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,70 @@
 <script>
-	export let name;
+  import Entry from './components/Entry.svelte';
+  let name = "Victor He";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style type="text/scss">
-	$primary: rgb(58, 161, 245);
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  $primary: #006089;
+  $background: #f1f4f6;
+  main {
+    .page-hero {
+      padding: 3rem 0 1rem;
+      text-align: center;
+      background-color: #ffffff;
+      #nav {
+        span {
+          display: inline-block;
+          padding: 0 4px;
+          a {
+            text-decoration: none;
+          }
+        }
+      }
+      h1 {
+        color: $primary;
+        font-size: 2.75rem;
+        font-weight: 700;
+        font-family: Roboto, sans-serif;
+      }
+    }
+    .divider {
+      position: relative;
+      top: -12px;
+      margin-left: -15%;
+      border-top: 1px solid $primary;
+      box-shadow: 41px 27px 0 25px $background;
+      -webkit-transform: rotate(-1deg);
+      -ms-transform: rotate(-1deg);
+      transform: rotate(-1deg);
+    }
+    .page-content {
+      //background-color: $background;
+    }
+  }
 
-	h1 {
-		color: $primary;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <div class="page-hero">
+    <div id="nav">
+      <span>
+        <a href="#blog">Blog</a>
+      </span>
+      <span>•</span>
+      <span>
+        <a href="#about">About</a>
+      </span>
+    </div>
+    <h1>{name}</h1>
+  </div>
+  <div class="divider" />
+  <div class="page-content">
+    <Entry/>
+  </div>
+  
+</main>
---
+++ src/components/Entry.svelte
@@ -0,0 +1,10 @@
+<style>
+  h2 {
+    color: rgb(99, 85, 66);
+  }
+</style>
+<div class="entry">
+  <div class="divider"></div>
+  <h2>SaaS Templatekkkk</h2>
+  <p>Everything you need</p>
+</div>
